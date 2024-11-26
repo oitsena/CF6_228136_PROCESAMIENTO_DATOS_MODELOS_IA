38f47ba4754753de9418d8dc7e21255c9178d1eb
nataliabecerraoit <natalia.becerra@bitinstitute.co>
2024-11-25 20:11:15 -0500

    se carga versión nueva

---
+++ actividades/story_content/user.js
@@ -0,0 +1,13 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  window.close();
+}
+
+};
